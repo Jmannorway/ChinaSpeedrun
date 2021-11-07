23dc16e6853ca853384f6dda544f6c8140364287
Jmannorway <jarklu@protonmail.com>
2021-11-07 21:19:34 +0100

    Tiny changes

--- ChinaSpeedrun/Component.cpp
+++ ChinaSpeedrun/Component.cpp
@@ -1,1 +1,24 @@
 #include "Component.h"
+
+cs::Component::Type cs::Component::GetType() const
+{
+	return type;
+}
+
+std::string cs::Component::GetTypeName() const
+{
+	return TypeToName(type);
+}
+
+std::string cs::Component::TypeToName(Type type)
+{
+	switch(type)
+	{
+	case Type::Audio:			return "Audio";
+	case Type::Camera:			return "Camera";
+	case Type::Physics:			return "Physics";
+	case Type::MeshRenderer:	return "MeshRenderer";
+	case Type::Transform:		return "Transform";
+	default:					return "";
+	}
+}
--- ChinaSpeedrun/Component.h
+++ ChinaSpeedrun/Component.h
@@ -1,13 +1,28 @@
 #pragma once
+#include <string>
 
 namespace cs
 {
 	class Component
 	{
 	public:
+		enum class Type
+		{
+			Transform,
+			Physics,
+			Camera,
+			Audio,
+			MeshRenderer
+		};
+
 		class GameObject* gameObject{ nullptr };
 		bool enabled{ true };
-		
 		virtual void ImGuiDrawComponent() = 0;
+
+		Type GetType() const;
+		std::string GetTypeName() const;
+		static std::string TypeToName(Type type);
+	protected:
+		Type type;
 	};
 }
